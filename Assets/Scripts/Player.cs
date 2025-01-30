2a09c28704b3cea09335bffb83e13780f5185c7c
soulduse <soulduse@naver.com>
2025-01-30 20:30:55 +0900

    애니메이션 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Settings")]
+    public float jumpForce;
+
+    [Header("References")]
+    public Rigidbody2D playerRigidbody;
+
+    private bool isGrounded = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRigidbody.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Platform")
+        {
+            isGrounded = true;
+        }
     }
 }
